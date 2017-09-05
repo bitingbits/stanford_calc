e2fa881e6d63367be612fb1bc16c7257fb822224
mac <mac@ws-165-49.han.nl>
2017-09-05 09:26:45 +0200

    Calculator

--- Calcularor/ViewController.swift
+++ Calcularor/ViewController.swift
@@ -9,17 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    let initialValue = "0"
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        print("\(digit) was called")
+        
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display!.text = textCurrentlyInDisplay + digit
+        } else {
+            display!.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var displayValue: Double{
+        get{
+            return Double(display.text!)!
+        }
+        set{
+            display.text = String(newValue)
+        }
     }
-
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = true
+        if let mathematicalSymbol = sender.currentTitle{
+            switch mathematicalSymbol{
+            case "π":
+                displayValue = Double.pi
+            case "√":
+                displayValue = sqrt(displayValue)
+            case "C":
+                userIsInTheMiddleOfTyping = false
+                display.text = initialValue
+            default:
+                break;
+                
+            }
+        }
+        
+        
+    }
 
 }
 
